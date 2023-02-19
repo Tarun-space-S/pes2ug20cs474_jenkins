157bab316ab5121724ab5e013233fca0e6cef5f9
Tarun S <89335295+Tarun-space-S@users.noreply.github.com>
2023-02-19 21:11:24 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hey! I am Sujan Rao R\n";
+  cout << "Hey! I am Tarun S\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
